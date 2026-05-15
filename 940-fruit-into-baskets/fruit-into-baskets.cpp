78cc8fcd74f2421b4559f34dfc558bd39d59adf7
himanshudevatwal03-lgtm <himanshudevatwal03@gmail.com>
2026-05-15 15:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 79.9 MB (94.85%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,20 +1,60 @@
 class Solution {
 public:
     int totalFruit(vector<int>& f) {
-        unordered_map<int,int> mp;
-        int left=0;
-        int ans=0;
-        for(int i=0;i<f.size();i++){
-            mp[f[i]]++;
-            while(mp.size()>2){
-                mp[f[left]]--;
-                if(mp[f[left]]==0){
-                    mp.erase(f[left]);
-                }
-                left++;
+        
+        if(f.size() <= 2) return f.size();
+
+        int b1 = 0;
+        int b2 = 0;
+
+        int lastFruit = -1;
+        int secondLastFruit = -1;
+
+        int countLast = 0;
+        int curr = 0;
+        int ans = 0;
+
+        for(int i = 0; i < f.size(); i++) {
+
+            if(f[i] == lastFruit || f[i] == secondLastFruit) {
+                curr++;
             }
-            ans=max(ans,i-left+1);
+            else {
+                curr = countLast + 1;
+            }
+
+            if(f[i] == lastFruit) {
+                countLast++;
+            }
+            else {
+                countLast = 1;
+                secondLastFruit = lastFruit;
+                lastFruit = f[i];
+            }
+
+            ans = max(ans, curr);
         }
+
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& f) {
+//         unordered_map<int,int> mp;
+//         int left=0;
+//         int ans=0;
+//         for(int i=0;i<f.size();i++){
+//             mp[f[i]]++;
+//             while(mp.size()>2){
+//                 mp[f[left]]--;
+//                 if(mp[f[left]]==0){
+//                     mp.erase(f[left]);
+//                 }
+//                 left++;
+//             }
+//             ans=max(ans,i-left+1);
+//         }
+//         return ans;
+//     }
+// };
