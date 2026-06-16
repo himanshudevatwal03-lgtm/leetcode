4cf1830acbe35eb16cace6b8e6cd6d5021b9afad
himanshudevatwal03-lgtm <himanshudevatwal03@gmail.com>
2026-06-16 15:46:26 +0530

    Time: 3 ms (76.89%) | Memory: 39.9 MB (24.24%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        for(char &c:s){
+            if(c=='*'){
+                if(res.size()){
+                    res.pop_back();
+                }
+            }else if(c=='#'){
+                res+=res;
+            }else if(c=='%'){
+                reverse(res.begin(),res.end());
+           }else{
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
