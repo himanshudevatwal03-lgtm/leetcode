95564ef82cef3ff2dc0c993d0a3b29ba480e4416
himanshudevatwal03-lgtm <himanshudevatwal03@gmail.com>
2026-06-24 15:51:49 +0530

    Time: 7 ms (65.30%) | Memory: 10.9 MB (81.63%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-        int atmostk(string nums, int k){
-        unordered_map<char,int>mpp;
-        int left=0,i=0;
-        int ans=0;
-        while(i<nums.size()){
-            mpp[nums[i]]++;
-            while(mpp.size()>k){
-                mpp[nums[left]]--;
-                if(mpp[nums[left]]==0) mpp.erase(nums[left]);
+    int numberOfSubstrings(string s) {
+        int a=0,b=0,c=0;
+        int ans=0,left=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a') a++;
+            else if(s[i]=='b') b++;
+            else c++;
+            while(a>=1 && b>=1 && c>=1){
+                 ans += s.size() - i;//aane wale charACTER KE SATH BHI JO SUBSTRING BANYEGA VO ABHI HI JOD LI
+                if(s[left]=='a') a--;
+                else if (s[left]=='b') b--;
+                else c--;
                 left++;
             }
-            ans+=i-left+1;
-            i++;
+
         }
         return ans;
-    }
-
-
-    int numberOfSubstrings(string s) {
-      return atmostk(s,3)-atmostk(s,2);
     }
 };
