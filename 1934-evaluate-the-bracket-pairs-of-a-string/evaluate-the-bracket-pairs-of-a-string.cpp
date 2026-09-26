d8156b315db53e6f0945eb9385b191feadda7e89
himanshudevatwal03-lgtm <himanshudevatwal03@gmail.com>
2026-09-26 18:55:43 +0530

    Time: 87 ms (43.92%) | Memory: 122.6 MB (76.01%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    string evaluate(string s, vector<vector<string>>& know) {
-        int n=s.size();
-        unordered_map<string,string>mp;
-        for(auto it:know){
-            mp[it[0]]=it[1];
-        }
-        string ans="";
-        for(int i=n-1;i>=0;i--){
-            string temp="";
-            if(s[i]==')'){
-                i--;
-                while(s[i]!='(' && i>=0 ){
-                    temp+=s[i];
-                    i--;
-                }
-                 reverse(temp.begin(), temp.end());
-                if(mp.find(temp)!=mp.end()){
-                    string val = mp[temp];
-                    reverse(val.begin(), val.end());
-                    ans += val;
-                }else{
-                    ans+='?';
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> mp;
+
+        for (auto &it : knowledge)
+            mp[it[0]] = it[1];
+
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                string key = "";
+                i++;
+
+                while (s[i] != ')') {
+                    key += s[i];
+                    i++;
                 }
 
-            }else
-                ans+=s[i];
-
+                if (mp.find(key) != mp.end())
+                    ans += mp[key];
+                else
+                    ans += '?';
+            }
+            else {
+                ans += s[i];
+            }
         }
-         reverse(ans.begin(), ans.end());
+
         return ans;
-        
     }
 };
